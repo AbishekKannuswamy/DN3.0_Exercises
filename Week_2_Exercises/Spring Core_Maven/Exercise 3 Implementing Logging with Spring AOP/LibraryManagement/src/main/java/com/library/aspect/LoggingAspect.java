ccaf5b657fa72c252ef9c0177aa0b1c7f9100df6
AbishekKannuswamy <abishekkannuswamy@gmail.com>
2024-08-08 19:04:23 +0530

    Week 2 Assignment

---
+++ Week_2_Exercises/Spring Core_Maven/Exercise 3 Implementing Logging with Spring AOP/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,27 @@
+package com.library.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.library.service.*.*(..))")
+    public void serviceLayer() {}
+
+    @Around("serviceLayer()")
+    public Object logExecutionTime(JoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        logger.info(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
---
+++ Week_2_Exercises/Spring Core_Maven/Exercise 6 Configuring Beans with Annotations/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,17 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+import com.library.repository.BookRepository;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void manageBooks() {
+        System.out.println("Managing books with " + bookRepository);
+    }
+}
