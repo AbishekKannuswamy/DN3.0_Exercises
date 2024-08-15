07eddf81475a4b579de662957f521e472e7aa9d0
AbishekKannuswamy <abishekkannuswamy@gmail.com>
2024-08-15 20:17:10 +0530

    3

--- Week_3_Exercises/Exercise 3/EmployeeManagementSystem/src/main/java/com/EmployeeManagementSystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.EmployeeManagementSystem.repository;
-import com.EmployeeManagementSystem.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Department findByName(String name);
-}
